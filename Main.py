838cec4a4030a2a58bb4e87845e9829cc80dc2bc
Jake Kim <75127141+Jakipy@users.noreply.github.com>
2022-02-24 23:39:52 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,15 +1,20 @@
 import random
 import string
+from numpy import square
 import pygame
 
 pygame.init()
+generation = 0
 random_row = 0
 random_col = 0
+food_location_collection = []
+food_location = []
+real_height = 600
 width = 500
 height = 500
 dimension = 50
-square_size = height/dimension
-screen = pygame.display.set_mode((width,height))
+square_size = height/dimension # in this case it is 10, 500/50 = 10
+screen = pygame.display.set_mode((width,real_height))
 white = (200, 200, 200)
 red = (255,   0,   0)
 green = (34,139,34) #forest green color
@@ -97,6 +102,7 @@ call_mutation = mutation()
 print("mutation_sub",call_mutation.sub())
 
 def drawGrid():
+    # the greed has a coordinate for (0,0) to (490,490)
     blockSize = int(square_size) #Set the size of the grid block
     for x in range(0, width, blockSize):
         for y in range(0, width, blockSize):
@@ -104,27 +110,66 @@ def drawGrid():
             pygame.draw.rect(screen, white, rect, 1)
 
 def draw_organism():
-    global random_row,random_col
-    food_location_collection = []
-    food_location = []
-
-    for i in range(20):
-        random_food_r =random.randint(1,50) * int(square_size)
-        random_food_c = random.randint(1,50) * int(square_size)
-        food_location.append(random_food_r)
-        food_location.append(random_food_c)
+    global random_row,random_col,food_location_collection,food_location
+    #colors = ["red","blue","yellow","orange","green","white"] # this color is for debuggin purpose later make sure to remove this color list and unite organism with one color. 
+    for i in range(6):
+        random_r =random.randint(1,49) * int(square_size)
+        random_c = random.randint(1,49) * int(square_size)
+        food_location = [random_r,random_c,]
         food_location_collection.append(food_location)
-        pygame.draw.rect(screen,green,pygame.Rect(random_food_c,random_food_r,int(square_size),int(square_size)))
-        pygame.draw.rect(screen,yellow,pygame.Rect(1,1,int(square_size),int(square_size)))
+        pygame.draw.rect(screen,white,pygame.Rect(random_r,random_c,int(square_size),int(square_size)))
+        #pygame.draw.rect(screen,yellow,pygame.Rect(1,1,int(square_size),int(square_size))) #also for debugging purpose
 
     return food_location
-def move_organism(speed):
-    pass
+def move_organism(food_location_collection,square_size,dimension):
+    print(food_location_collection)
+    organism_x_val = 0
+    organism_y_val = 0
+    
+    will_move = []
+    will_move_collection = []
+    for x in range(len(food_location_collection)):
+        for y in range(0,2):
+            if y == 0:
+                organism_x_val = dimension*10 - food_location_collection[x][y]
+            else:
+                organism_y_val = dimension*10 - food_location_collection[x][y]
 
+        if organism_x_val/10 == 49:
+            if organism_y_val/10 == 49: 
+                will_move = [0,0]
+                will_move_collection.append(will_move)
+            else:
+                will_move = [0,random.randint(organism_y_val/10,49)*int(square_size)]
+                will_move_collection.append(will_move)
+        else:
+            if organism_y_val/10 == 49:
+                will_move = [random.randint(organism_x_val/10,49)*int(square_size),0]
+                will_move_collection.append(will_move)
+
+            else:
+                will_move = [random.randint(organism_x_val/10,49)*int(square_size),random.randint(organism_y_val/10,49)*int(square_size)]
+                will_move_collection.append(will_move)
+
+        
+    print(will_move_collection)
+
+    for x in range(len(will_move_collection)):
+        pygame.draw.rect(screen,green,pygame.Rect(will_move_collection[x][0],will_move_collection[x][1],int(square_size),int(square_size)))
+
+
+       
+       
+
+            
+    
+    print(will_move_collection)
+    
 running = True
 draw_organism()
-
+move_organism(food_location_collection,square_size,dimension)
 while True:
+    
         drawGrid()
         
        
